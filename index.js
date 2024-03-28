c183b370d6acb623edd9c8d09dd3ca7b9b3b7dd5
Jaysh2007 <154827971+Jaysh2007@users.noreply.github.com>
2024-03-28 09:05:27 +0530

    filename changes

--- index.js
+++ index.js
@@ -1,52 +1,63 @@
-// import { MD5 } from "crypto-js/md5";
-// console.log(MD5("text to hash").toString());
-// let ts = new Date().toLocaleString('en-GB')
-// let ts = "17/03/2024, 17:09:39";
-// const pubKey = "4da64e69da4c8db76655129ae70e645a";
-// const privateKey = "587839a5396df452e2b55a83f287b59ca2a2b70c";
+let ts = "17/03/2024, 17:09:39";
+const pubKey = "4da64e69da4c8db76655129ae70e645a";
+const privateKey = "587839a5396df452e2b55a83f287b59ca2a2b70c";
+let searchHero = document.getElementById('search-string');
 
-// fetch(
-//   `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${pubKey}&hash=a65ab52659aa671b88ef9cd9ad448c4d`
-// )
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const realData = data.data.results;
-//     realData.forEach((user) => {
-//       const markup = `<a href='#${user.name}'><li id='${user.name}'>${user.name}</li></a>`;
-//       document.querySelector("ul").insertAdjacentHTML("beforeend", markup);
-//     });
-//   })
-//   .catch((error) => console.log(error));
 
-// const search = document.getElementById("searchbar");
-// search.addEventListener("input", () => {
-//   const searchResult = document.getElementsByClassName('searchResult');
-//   console.log(searchResult);
-//   if (searchResult.length != 0) Array.from(searchResult).forEach((e)=>{
-//     e.remove();
-//   });
-//   const searchItem = document.getElementById("ul");
-//   const keyword = document.getElementById("searchbar").value;
-//   fetch(
-//     `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${keyword}&ts=${ts}&apikey=${pubKey}&hash=a65ab52659aa671b88ef9cd9ad448c4d`
-//   )
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const searchData = data.data.results;
-//       searchData.forEach((user) => {
-//         const markup = `<div class="searchResult" ><li id='${user.name}'><a href="#">${user.name}</a></li></div>`;
-//         document
-//           .getElementById("floating-div")
-//           .insertAdjacentHTML("beforeend", markup);
-//       });
-//       document.getElementById("floating-div").style.display = "block";
-//     })
-//     .catch((error) => console.error(error));
-// });
 
-// const floatingDiv = document.getElementById("floating-div");
-// document.addEventListener("click", function (event) {
-//   if (!floatingDiv.contains(event.target)) {
-//     floatingDiv.style.display = "none";
-//   }
-// });
+searchHero.addEventListener("input", () => {
+    const keyword = document.getElementById("search-string").value;
+  fetch(
+    `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${keyword}&ts=${ts}&apikey=${pubKey}&hash=a65ab52659aa671b88ef9cd9ad448c4d`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const searchData = data.data.results;
+      searchData.forEach((user) => {
+        display(data)
+    })
+    .catch((error) => console.error(error));
+});
+})
+let canvas = document.getElementById('canvas');
+function display(data){
+
+    var superHeroList = document.getElementById('superhero-list');
+    superHeroList.innerHTML = "";
+    var results = data.data.results;
+    const searchName = document.getElementById('search-character');
+    if(!results){
+        searchName.value = "";
+        window.alert("No super hero found!");
+    }else{
+        for(let result of results){
+            console.log(result.thumbnail);
+
+            var templateCanvas = canvas.content.cloneNode(true);
+            templateCanvas.getElementById("image-sh").innerHTML = `<img class='image-hero' src="${result.thumbnail.path}.${result.thumbnail.extension}" />`;
+            templateCanvas.getElementById("name").innerHTML = '<b>Name: </b> ' + result.name;
+            templateCanvas.getElementById("id").innerHTML = '<b>Hero ID: </b> ' + result.id ;
+            templateCanvas.getElementById("comic").innerHTML = '<b>Comic Available: </b>'+ result.comics.available ;
+            templateCanvas.getElementById("series").innerHTML = '<b>Series Available: </b>'+ result.series.available ;
+            templateCanvas.getElementById("stories").innerHTML = '<b>Stories Available: </b>'+ result.stories.available ;
+            templateCanvas.getElementById('learn-more').addEventListener('click', function(){
+                localStorage.setItem('id', result.id);
+                window.location.assign('./about.html');
+            });
+            templateCanvas.getElementById('fav').addEventListener('click', function(){
+                var index = localStorage.length;
+                var data = JSON.stringify(result);
+                localStorage.setItem(result.id,data);
+
+            });
+            superHeroList.appendChild(templateCanvas);
+        }
+    }
+};
+function addFunction() {
+  var x = document.getElementById("fav-alert");
+  console.log('HERE')
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+ 
+}
